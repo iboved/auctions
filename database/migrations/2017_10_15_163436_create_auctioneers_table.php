dabaf1664e4ba1f1eefb971828ca160fd7058265
Volodymyr Gavrylov <iboved@gmail.com>
2017-10-15 20:02:14 +0300

    Create auctioneers table, Auctioneer model, AuctioneerController

---
+++ database/migrations/2017_10_15_163436_create_auctioneers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuctioneersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('auctioneers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->smallInteger('type');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('fax')->nullable();
+            $table->string('email')->nullable();
+            $table->string('site_url')->nullable();
+            $table->string('lat');
+            $table->string('lng');
+            $table->boolean('is_enabled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('auctioneers');
+    }
+}
