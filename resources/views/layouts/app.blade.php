6323d51460fbf09d50b4d6f9e9715cdf16c35cf1
Volodymyr Gavrylov <iboved@gmail.com>
2017-10-19 21:26:31 +0300

    Crop layout and create index page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,23 +42,11 @@
                 </div>
             </div>
         </nav>
-        <div class="container">
-            <div class="jumbotron">
-                <h1>Theme example</h1>
-                <p>This is a template showcasing the optional theme stylesheet included in Bootstrap. Use it as a starting point to create something more unique by building on or modifying it.</p>
-            </div>
 
-            <div class="row">
-                <div class="col-sm-12">
-                    <h2>What We Do</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A deserunt neque tempore recusandae animi soluta quasi? Asperiores rem dolore eaque vel, porro, soluta unde debitis aliquam laboriosam. Repellat explicabo, maiores!</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis optio neque consectetur consequatur magni in nisi, natus beatae quidem quam odit commodi ducimus totam eum, alias, adipisci nesciunt voluptate. Voluptatum.</p>
-                    <p>
-                        <a class="btn btn-primary btn-lg" href="#">Call to Action »</a>
-                    </p>
-                </div>
-            </div>
+        <div class="container">
+            @yield('content')
         </div>
+
         <footer class="footer">
             <div class="container">
                 <p class="text-center text-muted">Copyright © Your Website 2017</p>
