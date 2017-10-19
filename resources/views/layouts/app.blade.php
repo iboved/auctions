a20a57d674f29b969040611b9ec96c7620ce5048
Volodymyr Gavrylov <iboved@gmail.com>
2017-10-19 21:21:41 +0300

    Add assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         <title>Bootstrap 101 Template</title>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
     <body>
         <nav class="navbar navbar-inverse navbar-fixed-top">
@@ -20,7 +21,7 @@
                     </button>
                     <a class="navbar-brand" href="#">Bootstrap theme</a>
                 </div>
-                <div id="navbar" class="navbar-collapse collapse">
+                <div id="navbar" class="navbar-collapse collapse pull-right">
                     <ul class="nav navbar-nav">
                         <li class="active"><a href="#">Home</a></li>
                         <li><a href="#about">About</a></li>
@@ -48,7 +49,7 @@
             </div>
 
             <div class="row">
-                <div class="col-sm-8">
+                <div class="col-sm-12">
                     <h2>What We Do</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A deserunt neque tempore recusandae animi soluta quasi? Asperiores rem dolore eaque vel, porro, soluta unde debitis aliquam laboriosam. Repellat explicabo, maiores!</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis optio neque consectetur consequatur magni in nisi, natus beatae quidem quam odit commodi ducimus totam eum, alias, adipisci nesciunt voluptate. Voluptatum.</p>
@@ -56,38 +57,11 @@
                         <a class="btn btn-primary btn-lg" href="#">Call to Action »</a>
                     </p>
                 </div>
-                <div class="col-sm-4">
-                    <h2>Contact Us</h2>
-                    <address>
-                        <strong>Start Bootstrap</strong>
-                        <br>3481 Melrose Place
-                        <br>Beverly Hills, CA 90210
-                        <br>
-                    </address>
-                    <address>
-                        <abbr title="Phone">P:</abbr>
-                        (123) 456-7890
-                        <br>
-                        <abbr title="Email">E:</abbr>
-                        <a href="mailto:#">name@example.com</a>
-                    </address>
-                </div>
             </div>
         </div>
         <footer class="footer">
             <div class="container">
                 <p class="text-center text-muted">Copyright © Your Website 2017</p>
-                <ul class="list-inline">
-                    <li class="list-inline-item">
-                        <a href="#">Privacy</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#">Terms</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#">FAQ</a>
-                    </li>
-                </ul>
             </div>
         </footer>
         <script src="{{ asset('js/app.js') }}"></script>
