814cf92f4b54717b1a5600b70c1514c1f2c0f1ed
Volodymyr Gavrylov <iboved@gmail.com>
2017-10-22 22:34:22 +0300

    Change contacts route for compatibility with the original site

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('auctioneers', 'AuctioneerController@index')->name('auctioneers.index
 Route::get('auctioneers/{slug}', 'AuctioneerController@show')->name('auctioneers.show');
 Route::get('auction-galleries', 'AuctionGalleryController@index')->name('auction_galleries.index');
 Route::get('auction-galleries/{slug}', 'AuctionGalleryController@show')->name('auction_galleries.show');
-Route::get('contacts', 'ContactController@index')->name('contacts');
+Route::get('contact-us', 'ContactController@index')->name('contacts');
 Route::get('privacy-policy', 'HomeController@privacyPolicy')->name('privacy_policy');
 Route::post('feedback', 'ContactController@storeFeedback')->name('feedback.store');
 
